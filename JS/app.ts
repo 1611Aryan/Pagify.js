47b6fa2e32f24ac12d268ce196c60ec35f28fb03
Aryan <aryan2563737@gmail.com>
2020-12-06 23:24:16 +0530

    init function created which adds the multi page functionality to the form in just one line

--- JS/app.ts
+++ JS/app.ts
@@ -1,57 +1,70 @@
-const $ = (element: string) => {
-    return document.querySelector(element);
+const init = ({ time = '0.5s', curve = 'ease' } = {}) => {
+    addEventListener('resize', () => {
+        movePageBy = (<HTMLElement>document.querySelector('.pageContainer')).offsetWidth;
+    })
+
+    const prev = (<HTMLElement>document.querySelector('#previous_button'));
+    const next = (<HTMLElement>document.querySelector('#next_button'));
+    const submit = (<HTMLElement>document.querySelector('#submit_button'));
+    const form = (<HTMLElement>document.querySelector('#pageContainer'));
+    const noOfPage = document.getElementsByClassName('page').length;
+    //?Sets the value of numOfPage in css variables
+    const root = document.documentElement;
+    root.style.setProperty('--numOfPages', noOfPage.toString());
+    //?
+    //?Sets the transition time (which is an optional parameter) in css variables
+    root.style.setProperty('--pageTransitionTime', time);
+    //?
+    //?Sets the transition curve (which is an optional parameter) in css variables
+    root.style.setProperty('--pageTransitionCurve', curve);
+    //?
+    let posCounter = 0;
+    let movePageBy = (<HTMLElement>document.querySelector('.pageContainer')).offsetWidth;
+
+    const pageChange = (posCounter: number) => {
+        if (posCounter == 0) {
+            prev.style.display = 'none';
+        }
+        else {
+            prev.style.display = 'block';
+        }
+        if (posCounter == noOfPage - 1) {
+            next.style.display = 'none';
+            submit.style.display = 'block';
+        }
+        else {
+            next.style.display = "block";
+            submit.style.display = 'none';
+        }
+        let location = (posCounter * movePageBy / noOfPage);
+        form.style.transform = `translateX(-${location}px)`
+    }
+
+    prev.addEventListener('click', e => {
+        e.preventDefault();
+        posCounter--;
+        if (posCounter < 0) {
+            posCounter = 0;
+        }
+        else {
+            pageChange(posCounter);
+        }
+    });
+
+    next.addEventListener('click', e => {
+        e.preventDefault();
+        posCounter++;
+        if (posCounter > noOfPage - 1) {
+            posCounter = noOfPage - 1;
+        }
+        else {
+            pageChange(posCounter);
+        }
+    });
+
 }
 
-addEventListener('resize', () => {
-    movePageBy = (<HTMLElement>$('.pageContainer')).offsetWidth;
-})
 
-const prev = (<HTMLElement>$('#previous_button'));
-const next = (<HTMLElement>$('#next_button'));
-const submit = (<HTMLElement>$('#submit_button'));
-const form = (<HTMLElement>$('#pageContainer'));
-const noOfPage = document.getElementsByClassName('page').length;
-let posCounter = 0;
-let movePageBy = (<HTMLElement>$('.pageContainer')).offsetWidth;
+//?Pass a object with property of time and beizer curve for page transition time and transition curve default value is 0.5s and ease
 
-const pageChange = (posCounter: number) => {
-    if (posCounter == 0) {
-        prev.style.display = 'none';
-    }
-    else {
-        prev.style.display = 'block';
-    }
-    if (posCounter == noOfPage - 1) {
-        next.style.display = "none";
-        submit.style.display = 'block';
-    }
-    else {
-        next.style.display = "block";
-        submit.style.display = 'none';
-    }
-    let location = (posCounter * movePageBy / noOfPage);
-    form.style.transform = `translateX(-${location}px)`
-}
-
-prev.addEventListener('click', e => {
-    e.preventDefault();
-    posCounter--;
-    if (posCounter < 0) {
-        posCounter = 0;
-    }
-    else {
-        pageChange(posCounter);
-    }
-});
-
-next.addEventListener('click', e => {
-    e.preventDefault();
-    posCounter++;
-    let location = (posCounter * movePageBy / noOfPage);
-    if (posCounter > noOfPage - 1) {
-        posCounter = noOfPage - 1;
-    }
-    else {
-        pageChange(posCounter);
-    }
-});
+init();
